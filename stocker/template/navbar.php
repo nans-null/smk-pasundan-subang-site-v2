0f7c35c66372667aadf3870c8f9dc187652af9b6
nans-null <nans@smkpasundansubang.sch.id>
2025-09-17 21:14:14 +0700

    perbaikan navbar (submenu pada navbar menggunakan array), tobarstart di hidden

--- stocker/template/navbar.php
+++ stocker/template/navbar.php
@@ -1,6 +1,6 @@
 
 <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-    <a href="" class="navbar-brand p-0">
+    <a href="/" class="navbar-brand p-0">
         <h1 class="text-primary"><i class="fas fa-search-dollar me-3"></i>Stocker</h1>
         <!-- <img src="stocker/img/logo.png" alt="Logo"> -->
     </a>
@@ -9,61 +9,102 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto py-0">
-            <a href="index.html" class="nav-item nav-link <?= $active[0][0] ?>">Beranda</a>
+            <a href="/" class="nav-item nav-link <?= $active['beranda'] ?>">Beranda</a>
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link  <?= $active[1][0] ?>" data-bs-toggle="dropdown">
+                <a href="#" class="nav-link  <?= $active['profil'] ?>" data-bs-toggle="dropdown">
                     <span class="dropdown-toggle">Profil</span>
                 </a>
                 <div class="dropdown-menu m-0">
-                    <a href="feature.html" class="dropdown-item <?= $active[1][1] ?>">Sejarah</a>
-                    <a href="team.html" class="dropdown-item <?= $active[1][2] ?>">Identitas Sekolah</a>
-                    <a href="team.html" class="dropdown-item <?= $active[1][3] ?>">Lokasi</a>
-                    <a href="team.html" class="dropdown-item <?= $active[1][4] ?>">Galeri</a>
-                    <a href="team.html" class="dropdown-item <?= $active[1][5] ?>">Pegawai</a>
+                    <?php
+                        $dtprofil=[
+                                ["/profil/sejarah","Sejarah"],
+                                ["/profil/identitas","Identitas"],
+                                ["/profil/lokasi","Lokasi"],
+                                ["/profil/galeri","Galeri"],
+                                ["/profil/pegawai","Pegawai"]
+                            ];
+                    ?>
+                    <?php foreach($dtprofil as list($lilinkprofil,$linamaprofil)): ?>
+
+                    <a href="<?= $lilinkprofil ?>" class="dropdown-item"><?= $linamaprofil ?></a>
+
+                    <?php endforeach; ?>
+                    
                 </div>
             </div>
-            <a href="index.html" class="nav-item nav-link <?= $active[2][0] ?>">Program Sekolah</a>
+            <a href="index.html" class="nav-item nav-link <?= $active['progsek'] ?>">Program Sekolah</a>
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link <?= $active[3][0] ?>" data-bs-toggle="dropdown">
+                <a href="#" class="nav-link <?= $active['jurusan'] ?>" data-bs-toggle="dropdown">
                     <span class="dropdown-toggle">Jurusan</span>
                 </a>
                 <div class="dropdown-menu m-0">
-                    <a href="feature.html" class="dropdown-item <?= $active[3][1] ?>">Rekayasa Perangkat Lunak</a>
-                    <a href="team.html" class="dropdown-item <?= $active[3][2] ?>">Teknik Komputer dan Jaringan</a>
-                    <a href="testimonial.html" class="dropdown-item <?= $active[3][3] ?>">Akuntansi Lembaga Keuangan</a>
-                    <a href="offer.html" class="dropdown-item <?= $active[3][4] ?>">Manajemen Perkantoran</a>
-                    <a href="FAQ.html" class="dropdown-item <?= $active[3][5] ?>">Teknik Kendaraan Ringan</a>
-                    <a href="404.html" class="dropdown-item <?= $active[3][6] ?>">Perhotelan</a>
+                    <?php
+                        $dtjur=[
+                                ["/mjur/jurusan/rpl","Rekayasa Perangkat Lunak"],
+                                ["/mjur/jurusan/tkj","Teknik Komputer dan Jaringan"],
+                                ["/mjur/jurusan/tkr","Teknik Kendaraan Ringan"],
+                                ["/mjur/jurusan/ak","Akuntasi"],
+                                ["/mjur/jurusan/mp","Manajemen Perkantoran"],
+                                ["/mjur/jurusan/htl","Perhotelan"]
+                            ];
+                    ?>
+                    <?php foreach($dtjur as list($lilinkjur,$linamajur)): ?>
+
+                    <a href="<?= $lilinkjur ?>" class="dropdown-item"><?= $linamajur ?></a>
+
+                    <?php endforeach; ?>
+                    
                 </div>
             </div>
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link <?= $active[4][0] ?>" data-bs-toggle="dropdown">
+                <a href="#" class="nav-link <?= $active['kegsiswa'] ?>" data-bs-toggle="dropdown">
                     <span class="dropdown-toggle">Kegiatan Siswa</span>
                 </a>
                 <div class="dropdown-menu m-0">
-                    <a href="feature.html" class="dropdown-item <?= $active[4][1] ?>">OSIS</a>
-                    <a href="team.html" class="dropdown-item <?= $active[4][2] ?>">Pramuka</a>
-                    <a href="testimonial.html" class="dropdown-item <?= $active[4][3] ?>">PMR</a>
-                    <a href="offer.html" class="dropdown-item <?= $active[4][4] ?>">Futsal</a>
-                    <a href="FAQ.html" class="dropdown-item <?= $active[4][5] ?>">Muaythai</a>
-                    <a href="/pegawai-nano" class="dropdown-item <?= $active[4][6] ?>">Voli</a>
-                    <a href="404.html" class="dropdown-item <?= $active[4][7] ?>">Jurnalistik</a>
-                    <a href="404.html" class="dropdown-item <?= $active[4][8] ?>">Akustik</a>
-                    <a href="404.html" class="dropdown-item <?= $active[4][9] ?>">Karawitan</a>
-                    <a href="404.html" class="dropdown-item <?= $active[4][10] ?>">Tari</a>
+                    <?php
+                        $dtksiswa=[
+                                ["/mksiswa/kegiatan/osis","OSIS"],
+                                ["/mksiswa/kegiatan/pramuka","Pramuka"],
+                                ["/mksiswa/kegiatan/pmr","PMR"],
+                                ["/mksiswa/kegiatan/futsal","Futsal"],
+                                ["/mksiswa/kegiatan/voli","Bola Voli"],
+                                ["/mksiswa/kegiatan/jurnalistik","Jurnalistik"],
+                                ["/mksiswa/kegiatan/akustik","Akustik"],
+                                ["/mksiswa/kegiatan/karawitan","Karawitan"],
+                                ["/mksiswa/kegiatan/tari","Tari"]
+                            ];
+                    ?>
+                    <?php foreach($dtksiswa as list($lilinkdksiswa,$linamadksiswa)): ?>
+
+                    <a href="<?= $lilinkdksiswa ?>" class="dropdown-item"><?= $linamadksiswa ?></a>
+                    
+                    <?php endforeach; ?>
+
                 </div>
             </div>
             <div class="nav-item dropdown">
-                <a href="#" class="nav-link <?= $active[5][0] ?>" data-bs-toggle="dropdown">
+                <a href="#" class="nav-link <?= $active['konten'] ?>" data-bs-toggle="dropdown">
                     <span class="dropdown-toggle">Konten</span>
                 </a>
                 <div class="dropdown-menu m-0">
-                    <a href="feature.html" class="dropdown-item <?= $active[5][1] ?>">Berita</a>
-                    <a href="team.html" class="dropdown-item <?= $active[5][2] ?>">Artikel</a>
-                    <a href="team.html" class="dropdown-item <?= $active[5][3] ?>">Video</a>
+
+                    <?php
+                        $dtkonten=[
+                                ["/konten/berita","Berita"],
+                                ["/konten/artikel","Artikel"],
+                                ["/konten/video","Video"]
+                            ];
+                    ?>
+                    <?php foreach($dtkonten as list($lilinkkonten,$linamakonten)): ?>
+
+                    <a href="<?= $lilinkkonten ?>" class="dropdown-item"><?= $linamakonten ?></a>
+                    
+                    <?php endforeach; ?>
+
                 </div>
             </div>
-            <a href="contact.html" class="nav-item nav-link <?= $active[6][0] ?>">Kontak Kami</a>
+            <a href="contact.html" class="nav-item nav-link <?= $active['kontak'] ?>">Kontak Kami</a>
+            <a href="contact.html" class="nav-item nav-link <?= $active['login'] ?>">Login <i class="fa fa-sign-in-alt"></i></a>
         </div>
     </div>
 </nav>
--- stocker/template/topbarstart.php
+++ stocker/template/topbarstart.php
@@ -1,3 +1,4 @@
+<!--
 <div class="container-fluid topbar bg-light px-5 d-none d-lg-block">
             <div class="row gx-0 align-items-center">
                 <div class="col-lg-8 text-center text-lg-start mb-2 mb-lg-0">
@@ -14,3 +15,4 @@
                 </div>
             </div>
         </div>
+-->
